1b2fa6f8d86910bc24f162640de2a9c47fd2bdd9
Lanhuong29 <lh98mk@gmail.com>
2023-03-26 23:27:08 +0700

    26/3

---
+++ Session9/app.js
@@ -0,0 +1,12 @@
+// alert('JavaScript External');
+
+var tenBien; //Kiểu Camel Case
+var tien_bien; //Kiểu Snake
+var ten, tuoi, diaChi; //2 ký tự được phép sử dụng trong khai báo tên biến là "$" và "_"
+var so = 12345, chuoi = "Chuỗi", chuoi1 = 'Chuỗi 1', chuoi2 = `Chuỗi 2`, check = false, $null = null, _undf = undefined; //Có 3 cách nhập chuỗi, số là "", '', ``
+
+alert(typeof so);
+alert(typeof chuoi);
+alert(typeof check);
+alert(typeof $null);
+alert(typeof _undf);
